1451ba8e077b1dcf82494f4535852e19477b031d
Markides.Markellos <markellos.markides@gmail.com>
2026-06-16 10:59:18 +0300

    Add footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,34 @@
+import './footer.css';
+import { createElement } from '../../utils/dom.js';
+
+export function createFooter() {
+  const footer = createElement('footer', {
+    classNames: ['site-footer'],
+  });
+
+  const copyright = createElement('p', {
+    classNames: ['site-footer__copyright'],
+    text: `© ${new Date().getFullYear()} Markellos Ecosystem`,
+  });
+
+  const navigation = createElement('nav', {
+    classNames: ['site-footer__navigation'],
+    attributes: { 'aria-label': 'Legal information' },
+  });
+
+  navigation.append(
+    createElement('a', {
+      classNames: ['site-footer__link'],
+      text: 'Privacy',
+      attributes: { href: '/privacy/' },
+    }),
+    createElement('a', {
+      classNames: ['site-footer__link'],
+      text: 'Cookies',
+      attributes: { href: '/cookies/' },
+    }),
+  );
+
+  footer.append(copyright, navigation);
+  return footer;
+}
