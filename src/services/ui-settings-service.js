44f862653b35c74086846b3fe692b2b3f8195897
Markides.Markellos <markellos.markides@gmail.com>
2026-06-16 17:45:04 +0300

    Lock existing saved layouts on load

--- src/services/ui-settings-service.js
+++ src/services/ui-settings-service.js
@@ -58,7 +58,10 @@ export function getUiSettings() {
   }
 
   try {
-    return normalizeSettings(JSON.parse(storedValue));
+    return {
+      ...normalizeSettings(JSON.parse(storedValue)),
+      positionMode: DEFAULT_UI_SETTINGS.positionMode,
+    };
   } catch {
     return { ...DEFAULT_UI_SETTINGS };
   }
