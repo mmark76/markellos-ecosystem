8c0c3c1207c76576728a1a70774bf537a439bee8
Markides.Markellos <markellos.markides@gmail.com>
2026-06-16 22:23:02 +0300

    Load interface scale on legal pages

--- src/pages/legal-page.js
+++ src/pages/legal-page.js
@@ -3,6 +3,7 @@ import '../styles/tokens.css';
 import '../styles/global.css';
 import '../components/ui-settings/ui-settings.css';
 import './legal-page.css';
+import '../styles/default-scale.css';
 
 import { applyUiSettings } from '../services/ui-settings-service.js';
 
