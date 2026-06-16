6d0ed2c0f5bcfc9455a045c73b0ad9fd75f17bb8
Markides.Markellos <markellos.markides@gmail.com>
2026-06-16 23:20:47 +0300

    Split Personal Thoughts label into three lines

--- src/components/project-node/project-node.js
+++ src/components/project-node/project-node.js
@@ -1,6 +1,34 @@
 import './project-node.css';
 import { createElement } from '../../utils/dom.js';
 
+const PROJECT_LABEL_LINES = Object.freeze({
+  'personal-thoughts-and-writings': ['Personal', 'Thoughts and', 'Writings'],
+});
+
+function createProjectLabel(project) {
+  const label = createElement('span', {
+    classNames: ['project-node__label'],
+    attributes: { 'aria-label': project.title },
+  });
+  const lines = PROJECT_LABEL_LINES[project.id];
+
+  if (!lines) {
+    label.textContent = project.title;
+    return label;
+  }
+
+  lines.forEach((line) => {
+    label.append(
+      createElement('span', {
+        classNames: ['project-node__label-line'],
+        text: line,
+      }),
+    );
+  });
+
+  return label;
+}
+
 export function createProjectNode(project) {
   const node = createElement('a', {
     classNames: ['project-node', `project-node--${project.position}`],
@@ -12,12 +40,7 @@ export function createProjectNode(project) {
     },
   });
 
-  node.append(
-    createElement('span', {
-      classNames: ['project-node__label'],
-      text: project.title,
-    }),
-  );
+  node.append(createProjectLabel(project));
 
   return node;
 }
