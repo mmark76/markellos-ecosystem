5dbdbf715b3153dcde5671dddeae50c0936722cc
Markides.Markellos <markellos.markides@gmail.com>
2026-06-16 23:16:25 +0300

    Load selectable font styles on legal pages

--- src/pages/legal-page.js
+++ src/pages/legal-page.js
@@ -4,6 +4,7 @@ import '../styles/global.css';
 import '../components/ui-settings/ui-settings.css';
 import './legal-page.css';
 import '../styles/default-scale.css';
+import '../styles/font-options.css';
 
 import { applyUiSettings } from '../services/ui-settings-service.js';
 
